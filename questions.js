5345625b78eb9c2436a55453182f262c612a743d
adityabhosle-1 <68443991+adityabhosle-1@users.noreply.github.com>
2021-05-10 14:39:10 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,128 @@
+alert("Hello");
+// Creating an array and passing the number, questions, options and answers
+
+let questions = [
+   {
+     numb: 1,
+     question: "What is the name of Thor's hammer?",
+     answer: "Mjölnir",
+     options: [
+         "Vanir",
+         "Mjölnir",
+         "Aesin",
+         "Norn"
+     ]
+
+   },
+   {
+     numb: 2,
+     question: "Which Material is Captain America's shield made of?",
+     answer: "Vibranium",
+     options: [
+         "Adamantium",
+         "Vibranium",
+         "Praesodymium",
+         "Carbonadium"
+     ]
+
+   },
+   {
+     numb: 3,
+     question: "What is the real name of The Black Panther?",
+     answer: "T'challa",
+     options: [
+         "N'jobu",
+         "M'baku",
+         "T'challa",
+         "W'Jadaka"
+     ]
+
+   },
+   {
+     numb: 4,
+     question: "What is the name of Black Panther's sister?",
+     answer: "Shuri",
+     options: [
+         "Nakiya",
+         "Ramonda",
+         "Okoye",
+         "Shuri"
+     ]
+
+   },
+   {
+     numb: 5,
+     question: "Who played hulk before mark ruffalo?",
+     answer: "Edward Norton",
+     options: [
+         "Marlon Brando",
+         "Gary Oldman",
+         "Edward Norton",
+         "Charles Mountbatten"
+     ]
+
+   },
+   {
+     numb: 6,
+     question: "Who is Thor's father?",
+     answer: "Odin",
+     options: [
+         "Loki",
+         "Hela",
+         "Zelda",
+         "Odin"
+     ]
+
+   },
+   {
+     numb: 7,
+     question: "What do the avengers eat after the battle of New York?",
+     answer: "Shwarmas",
+     options: [
+         "Burgers",
+         "A Pizza",
+         "Bratwurst",
+         "Shwarmas"
+     ]
+
+   },
+   {
+     numb: 8,
+     question: "Who is the main villian in Spiderman: Homecoming?",
+     answer: "The Vulture",
+     options: [
+         "Mysterio",
+         "The Vulture",
+         "Venom",
+         "Carnage"
+     ]
+
+   },
+   {
+     numb: 9,
+     question: "Which war did Captian America fight in?",
+     answer: "WW2",
+     options: [
+         "WW2",
+         "WW1",
+         "The 100 years war",
+         "The Emu war"
+     ]
+
+   },
+   {
+     numb: 10,
+     question: "Which of the following movies came first?",
+     answer: "Ant-Man",
+     options: [
+         "Ant-Man",
+         "Guardians of the galaxy 2",
+         "Dr. Strange",
+         "Captain America: Civil War"
+     ]
+
+   },
+
+
+
+];
